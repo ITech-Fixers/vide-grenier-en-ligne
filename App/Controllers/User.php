f6312838000b59c163c0539c78687d9b499deae5
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-05-30 20:24:22 +0200

    fix exception handling for login and register in the controller

--- App/Controllers/User.php
+++ App/Controllers/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Controllers;
 
+use App\Exception\UserNotFoundException;
+use App\Exception\ValidationException;
 use App\Models\UserRegister;
 use App\Models\Articles;
 use App\Utility\Flash;
 use App\Utility\Hash;
 use Core\Controller;
 use Core\View;
-use ErrorException;
 use Exception;
 use Random\RandomException;
 
@@ -22,54 +23,68 @@ class User extends Controller
     {
         parent::__construct($route_params);
     }
+
     /**
      * Affiche la page de login
      * @throws Exception
      */
     public function loginAction(): void
     {
-        if(isset($_POST['submit'])){
-            $request = $_POST;
+        try {
+            if(isset($_POST['submit'])){
+                $request = $_POST;
 
-            if (!isset($request['email']) || !isset($request['password'])){
-                Flash::danger('Veuillez renseigner un email et un mot de passe');
-                View::renderTemplate('User/login.html');
-                return;
+                if (!isset($request['email']) || !isset($request['password'])){
+                    Flash::danger('Veuillez renseigner un email et un mot de passe');
+                    View::renderTemplate('User/login.html');
+                    return;
+                }
+
+                $this->login($request);
+
+                header('Location: /account');
             }
 
-            $this->login($request);
-
-            header('Location: /account');
+            View::renderTemplate('User/login.html');
+        } catch (ValidationException|UserNotFoundException $e) {
+            Flash::danger($e->getMessage());
+            View::renderTemplate('User/login.html');
+        } catch (Exception) {
+            Flash::danger('Une erreur est survenue, veuillez réessayer');
+            View::renderTemplate('User/login.html');
         }
-
-        View::renderTemplate('User/login.html');
     }
 
     /**
      * Page de création de compte
-     * @throws ErrorException
      * @throws Exception
      */
     public function registerAction(): void
     {
-        if(isset($_POST['submit'])){
-            $request = $_POST;
-            $errors = UserRegister::validate($request);
+        try {
+            if(isset($_POST['submit'])){
+                $request = $_POST;
+                $errors = UserRegister::validate($request);
 
-            if (!empty($errors)){
-                Flash::danger(implode('<br>', $errors));
-                View::renderTemplate('User/register.html');
+                if (!empty($errors)){
+                    throw new ValidationException(implode('<br>', $errors));
+                }
+
+                $this->register($request);
+                $this->login($request);
+
+                View::renderTemplate('User/account.html');
                 return;
             }
 
-            $this->register($request);
-            $this->login($request);
-
-            View::renderTemplate('User/account.html');
-            return;
+            View::renderTemplate('User/register.html');
+        } catch (ValidationException $e) {
+            Flash::danger($e->getMessage());
+            View::renderTemplate('User/register.html');
+        } catch (UserNotFoundException|Exception) {
+            Flash::danger('Une erreur est survenue, veuillez réessayer');
+            View::renderTemplate('User/register.html');
         }
-
-        View::renderTemplate('User/register.html');
     }
 
     /**
@@ -90,55 +105,46 @@ class User extends Controller
      */
     private function register($data): void
     {
-        try {
-            // Generate a salt, which will be applied to the during the password
-            // hashing process.
-            $salt = Hash::generateSalt(32);
+        // Generate a salt, which will be applied to the during the password
+        // hashing process.
+        $salt = Hash::generateSalt(32);
 
-            \App\Models\User::createUser([
-                "email" => $data['email'],
-                "username" => $data['username'],
-                "password" => Hash::generate($data['password'], $salt),
-                "salt" => $salt
-            ]);
-
-            return;
-
-        } catch (Exception $ex) {
-            Flash::danger($ex->getMessage());
-        }
+        \App\Models\User::createUser([
+            "email" => $data['email'],
+            "username" => $data['username'],
+            "password" => Hash::generate($data['password'], $salt),
+            "salt" => $salt
+        ]);
     }
 
+    /**
+     * @throws ValidationException
+     * @throws UserNotFoundException
+     * @throws RandomException
+     */
     private function login($data): void
     {
-        try {
-            if(!isset($data['email'])){
-                throw new Exception('Veuillez renseigner un email');
-            }
+        if(!isset($data['email'])){
+            throw new ValidationException('Veuillez renseigner un email');
+        }
 
-            $user = \App\Models\User::getByLogin($data['email']);
+        $user = \App\Models\User::getByLogin($data['email']);
 
-            if (!$user) {
-                throw new Exception('Utilisateur non trouvé');
-            }
+        if (!$user) {
+            throw new UserNotFoundException('Utilisateur non trouvé');
+        }
 
-            if (Hash::generate($data['password'], $user['salt']) !== $user['password']) {
-                throw new Exception('Mot de passe incorrect');
-            }
+        if (Hash::generate($data['password'], $user['salt']) !== $user['password']) {
+            throw new ValidationException('Mot de passe incorrect');
+        }
 
-            $_SESSION['user'] = array(
-                'id' => $user['id'],
-                'username' => $user['username'],
-            );
+        $_SESSION['user'] = array(
+            'id' => $user['id'],
+            'username' => $user['username'],
+        );
 
-            if (isset($data['remember-me'])) {
-                $this->createRememberMeToken($user['id']);
-            }
-
-            return;
-
-        } catch (Exception $ex) {
-            Flash::danger($ex->getMessage());
+        if (isset($data['remember-me'])) {
+            $this->createRememberMeToken($user['id']);
         }
     }
 
@@ -185,10 +191,9 @@ class User extends Controller
         $token = bin2hex(random_bytes(32));
         $expiresAt = date('Y-m-d H:i:s', strtotime('+30 days'));
 
-        // Insérer le token dans la base de données
         \App\Models\User::storeRememberMeToken($userId, $token, $expiresAt);
 
-        // Créer le cookie avec une durée de vie de 30 jours
+        // Cookie avec une durée de vie de 30 jours
         setcookie('remember_me', $token, time() + (86400 * 30), "/", "", false, true);
     }
 }
