71eb38a8637f78eb20a0e6439226c6fc92c439a4
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-07-09 15:24:20 +0200

    ajout des actions controller

--- App/Controllers/Product.php
+++ App/Controllers/Product.php
@@ -2,12 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Exception\ArticleNotFoundException;
 use App\Exception\CityNotFoundException;
+use App\Exception\MailerException;
+use App\Exception\UserNotFoundException;
 use App\Exception\ValidationException;
 use App\Models\ArticleAdd;
 use App\Models\Articles;
 use App\Models\Cities;
+use App\Models\ContactMessage;
+use App\Models\User;
 use App\Utility\Flash;
+use App\Utility\Mailer;
 use App\Utility\Upload;
 use Core\Controller;
 use Core\View;
@@ -76,13 +82,107 @@ class Product extends Controller
             $suggestions = Articles::getSuggest();
             $article = Articles::getOne($id);
 
+            if (empty($article)) {
+                throw new ArticleNotFoundException('L\'article n\'existe pas');
+            }
+
             View::renderTemplate('Product/Show.html', [
                 'article' => $article[0],
                 'suggestions' => $suggestions
             ]);
+        } catch (ArticleNotFoundException $e) {
+            Flash::danger($e->getMessage());
+            header ("Location: /");
         } catch (Exception) {
             Flash::danger('Une erreur est survenue, veuillez réessayer');
             header ("Location: /");
+        }
+    }
+
+    /**
+     * Affiche la page de contact
+     * @return void
+     * @throws Exception
+     */
+    public function contactAction(): void
+    {
+        $articleId = $this->route_params['id'];
+        $article = Articles::getOne($articleId);
+
+        try {
+            if (empty($article)) {
+                throw new ArticleNotFoundException('L\'article n\'existe pas');
+            }
+
+            $owner = User::getByArticle($articleId);
+
+            if (empty($owner)) {
+                throw new UserNotFoundException('Le détenteur de l\'article n\'existe pas');
+            }
+
+            View::renderTemplate('Product/Contact.html', [
+                'owner' => $owner,
+                'article' => $article[0]
+            ]);
+        } catch (ArticleNotFoundException|UserNotFoundException $e) {
+            Flash::danger($e->getMessage());
+            header ("Location: /");
+        } catch (Exception $e) {
+            Flash::danger('Une erreur est survenue, veuillez réessayer');
+            header ("Location: /");
+        }
+    }
+
+    /**
+     * Envoie un message à l'utilisateur
+     * @return void
+     * @throws Exception
+     */
+    public function sendMessageAction(): void
+    {
+        $articleId = $this->route_params['id'];
+
+        try {
+            $article = Articles::getOne($articleId);
+
+            if (empty($article)) {
+                throw new ArticleNotFoundException('L\'article n\'existe pas');
+            }
+
+            $owner = User::getByArticle($articleId);
+            $user = User::getById($_SESSION['user']['id']);
+
+            if (empty($owner) || empty($user)) {
+                empty($owner) ? $message = 'Le détenteur de l\'article n\'existe pas' : $message = 'L\'utilisateur n\'existe pas';
+                throw new UserNotFoundException($message);
+            }
+
+            $request = $_POST;
+            $errors = ContactMessage::validate($request);
+
+            if (!empty($errors)) {
+                throw new ValidationException(implode('<br>', $errors));
+            }
+
+            Mailer::send(
+                $user['username'],
+                $user['email'],
+                $owner['username'],
+                $owner['email'],
+                $request['libellé'],
+                $request['message']
+            );
+
+            Articles::addOneContact($articleId);
+
+            Flash::success('Message envoyé avec succès');
+            header ("Location: /product/$articleId");
+        } catch (ArticleNotFoundException|ValidationException|UserNotFoundException|MailerException $e) {
+            Flash::danger($e->getMessage());
+            header ("Location: /product/$articleId");
+        } catch (Exception) {
+            Flash::danger('Une erreur est survenue, veuillez réessayer');
+            header ("Location: /product/$articleId");
         }
     }
 }
